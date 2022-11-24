0c0191c930929121ff3409189661938a5fb93cfc
ashis-solomon <ashissolomon24@gmail.com>
2022-11-24 23:30:03 +0530

    update

--- static/js/app.js
+++ static/js/app.js
@@ -27,6 +27,16 @@ let search_trackUrl = [];
 let search_ReleaseDate = [];
 let search_display = [];
 
+// Splash Screen
+
+const splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded',(e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none');
+    },2000);
+})
+
 
 // GENERATE ACCESS-TOKEN USING REFRESH-TOKEN
 
@@ -55,10 +65,22 @@ function handleAuthorizationResponse() {
 
 function onPageLoad(){
     console.log('PageLoad');
+
+    if( window.localStorage ){
+    if( !localStorage.getItem('firstLoad') ){
+      localStorage['firstLoad'] = true;
+      window.location.reload();
+    }  
+    else{
+        localStorage.removeItem('firstLoad');
+    }
+  }
+    
+  
     refreshAccessToken();
     setTimeout(() => {  
         access_token = window.sessionStorage.getItem('access_token');
-        enable_newReleases(); }, 400);
+        enable_newReleases(); }, 1000);
     // enable_newReleases();
 }
 
@@ -89,7 +111,7 @@ async function enable_searchItem(query) {
     let market = 'US'
     let url = `${SEARCH_URL}?q=${query}&type=track&market=${market}&limit=${limit}&offset=${offset}`
     let resp = await searchItem(url)
-    console.log(1)
+
     console.log(resp)
     
     for(let i=0; i<resp['tracks']['items'].length; i++){
@@ -134,18 +156,7 @@ async function enable_searchItem(query) {
         }
         search_display[i] = search_trackNames[i] + ' - ' + artist_str
     }
-    // for(let i = 0; i < 6; i++){
-    //     suggest_list.children[i].text = 'a';
-    // }
-    for(let i = 0; i < search_display.length; i++){
-        console.log(search_display[i])
-        suggest_list.children[i].text = search_display[i];
-    }
-    console.log('---------------')
-    for(let i = 0; i < search_display.length; i++){
-        console.log(suggest_list.children[i].text)
-    }
-    console.log(2)
+    
 }
     
 function resolve_searchID(str){
@@ -153,31 +164,5 @@ function resolve_searchID(str){
         if(search_display[i] == str){
             return i;
         }
-    }
-}
-
-// keyUP Listener on SeachBox
-
-inputTrack.addEventListener('keyup', keyfunc)
-
-function keyfunc(e) {
-    e.preventDefault();
-    if(inputTrack.value != ''){
-        enable_searchItem(inputTrack.value)
-        //suggestions1 = search_display
-        let resolved_id = resolve_searchID(inputTrack.value)
-        localStorage.setItem('trackDisplayName',search_display[resolved_id])
-        localStorage.setItem('trackID',search_trackIDs[resolved_id])
-        localStorage.setItem('artistIDs',search_artistIDs[resolved_id])
-        localStorage.setItem('trackExplicit',search_trackExplicit[resolved_id])
-        localStorage.setItem('trackReleaseDate',search_ReleaseDate[resolved_id])
-        localStorage.setItem('trackPopularity',search_trackPopularity[resolved_id])
-        localStorage.setItem('trackUrl',search_trackUrl[resolved_id])
-
-        let route_pass = search_trackIDs[resolved_id] + ',' + search_trackNames[resolved_id] + ',' + search_trackPopularity[resolved_id]
-        localStorage.setItem('routePass',route_pass)
-        
-        input_hidden.value = route_pass
-        // console.log(`hiddenvalue  -  ${input_hidden.value}`)
     }
 }
--- static/js/new.js
+++ static/js/new.js
@@ -2,6 +2,15 @@
 // refreshAccessToken()
 access_token = window.sessionStorage.getItem('access_token');
 
+let artistidsx = {}
+let artistnamesx={}
+const songx_ids=[]
+const songx_names=[]
+const songx_albPop=[]
+const songx_explicit=[]
+const songx_trkURL=[]
+
+
 
 async function newReleases(url){
     let response = await fetch(url, {
@@ -16,41 +25,106 @@ async function newReleases(url){
 }
 
 async function enable_newReleases(){
+    let albumstring=''
+    const songimgs=[]
     let offset = 0;
     let limit = 12;
     let market = 'US';
-    let url = `${RELEASE_URL}?country=${market}&limit=${limit}&offset=${offset}`;
-    let resp = await searchItem(url);
+    let url = `https://api.spotify.com/v1/browse/new-releases?country=${market}&limit=${limit}&offset=${offset}`;
+    let resp = await newReleases(url);
+
+    // console.log(resp)
 
     let albums = resp['albums']
+    // console.log(albums)
     let items = albums['items']
+    // console.log(items)
+    for(let i=0;i<items.length;i++){
+         albumstring=albumstring+items[i]["id"]+'%2C'
+         songimgs.push(items[i]["images"][1]["url"])
+    }
+    // console.log(albumstring)
+    albumstring=albumstring.slice(0, -3);
+    // console.log(albumstring)
+    
+    let albumurl=`https://api.spotify.com/v1/albums?ids=${albumstring}&market=US`
+    // console.log(albumurl)
 
-    for(let iter=0; iter<12; iter++){
-        artists = items[iter]['artists'];
-        let str='';
-        for(let i=0; i<artists.length; i++){
-            if(i==0){
-                str = str + artists[i]['name'];
-            }else{
-                str = str + ", " +artists[i]['name']
-            }
+    //https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX%2C1A2GTWGtFfWp7KSQTwWOyo%2C2noRn2Aes5aoNVsU6iWThc&market=US
+
+
+    enablex_trackids(albumurl,songimgs)
+}
+
+
+async function getx_trackids(url,access_token) {
+    let response = await fetch(url, {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + access_token,
         }
+    });
+    let data = await response.json()
+    return data;
+}
+
+async function enablex_trackids(albumurl,songimgs) {
+   
+    let artist_string=''
+    let artist_name_string=''
+    let jsondata = await getx_trackids(albumurl,access_token)  
+    console.log(jsondata)
+
+    
+
+    // console.log(jsondata["albums"][0]["tracks"]["items"][12]) 
+    for(let i=0;i<jsondata["albums"].length;i++){
+        let tempLEN = jsondata["albums"][i]["tracks"]["items"].length - 1;
+        songx_ids.push(jsondata["albums"][i]["tracks"]["items"][tempLEN]["id"])
+        songx_names.push(jsondata["albums"][i]["tracks"]["items"][tempLEN]["name"])
+        songx_albPop.push(jsondata["albums"][i]["popularity"])
+        songx_explicit.push(jsondata["albums"][i]["tracks"]["items"][tempLEN]["explicit"])
+        songx_trkURL.push(jsondata["albums"][i]["tracks"]["items"][tempLEN]["external_urls"]["spotify"])
+    }
+    // console.log(songx_ids)
+    // console.log(songx_names)
+    for(let i=0;i<jsondata["albums"].length;i++){
+        let tempLEN = jsondata["albums"][i]["tracks"]["items"].length - 1;
+        artist_string=''
+        artist_name_string=''
+        for(let j=0;j<jsondata["albums"][i]["tracks"]["items"][tempLEN]["artists"].length;j++){
+            artist_string=artist_string+jsondata["albums"][i]["tracks"]["items"][tempLEN]["artists"][j]["id"]+","
+            artist_name_string=artist_name_string+jsondata["albums"][i]["tracks"]["items"][tempLEN]["artists"][j]["name"]+", "
+        }
+        artistidsx[i]=artist_string.slice(0,-1)
+        artistnamesx[i]=artist_name_string.slice(0,-2)
+    }
+    // console.log(artistidsx)
+    // console.log(artistidsx[0])
+    // console.log(artistnamesx)
+    // console.log(songimgs)
+    for(let iter=0; iter<12; iter++){
+       
        
         let selector_id = `card${iter}`;
         var x = document.getElementById(`${selector_id}`);
-        x.querySelector("h5").innerHTML = items[iter]['name'];
-        x.querySelector("p").innerHTML = str;
-        x.querySelector("img").alt = items[iter]['name'];
-        x.querySelector("img").src = items[iter]['images'][1]['url'];
+        x.querySelector("h5").innerHTML = songx_names[iter];
+        x.querySelector("p").innerHTML = artistnamesx[iter]
+        x.querySelector("img").alt = songx_names[iter];
+        x.querySelector("img").src = songimgs[iter];
         x.addEventListener("click",function(e){
             // alert("clicked");
             e.preventDefault();
-
-            inputTrack.value = items[iter]['name']
+            set_NEWvalue(iter);
+            // inputTrack.value = songx_names[iter]
+            //check this lineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
             
-            var uuuu = "http://127.0.0.1:5000/audio?name=newReleases" + inputTrack.value + items[iter]['id'];
-            var uu = "http://127.0.0.1:5000/audio?name=newReleases&trk="+`${items[iter]['id']}`;
-            location.href = uu;
+            // let uuu = `https://open.spotify.com/track/${songx_ids[iter]}`;
+            // location.href = uuu;
+
+            let webLink = `/audio?displayName=${songx_names[iter]}%20-%20${artistnamesx[iter]}&trkID=${songx_ids[iter]}&trkPop=${songx_albPop[iter]}`;
+            location.href = webLink;
 
             enable_searchItem(items[iter]['name'])
             console.log('a')
@@ -68,99 +142,49 @@ async function enable_newReleases(){
         // x.querySelector("a").target = "_blank"; 
 
     } 
+    
+    //return avg_value_dict
+
+}
+
+function set_NEWvalue(resolved_id){
+    localStorage.setItem('trackDisplayName',`${songx_names[resolved_id]} - ${artistnamesx[resolved_id]}`)
+    localStorage.setItem('trackID',songx_ids[resolved_id])
+    localStorage.setItem('artistIDs',artistidsx[resolved_id])
+    localStorage.setItem('trackExplicit',songx_explicit[resolved_id])
+    // localStorage.setItem('trackReleaseDate',search_ReleaseDate[resolved_id])
+
+    // localStorage.setItem('trackPopularity',songx_albPop[resolved_id])
+    //temporarily giving album pop as trk pop
+
+    localStorage.setItem('trackUrl',songx_trkURL[resolved_id])    
+
+    // let route_pass = search_trackIDs[resolved_id] + ',' + search_trackNames[resolved_id] + ',' + search_trackPopularity[resolved_id]
+    // localStorage.setItem('routePass',route_pass)
         
 }
 
-function set_value(resolved_id){
-    localStorage.setItem('trackDisplayName',search_display[resolved_id])
-    localStorage.setItem('trackID',search_trackIDs[resolved_id])
-    localStorage.setItem('artistIDs',search_artistIDs[resolved_id])
-    localStorage.setItem('trackExplicit',search_trackExplicit[resolved_id])
-    localStorage.setItem('trackReleaseDate',search_ReleaseDate[resolved_id])
-    localStorage.setItem('trackPopularity',search_trackPopularity[resolved_id])
-    localStorage.setItem('trackUrl',search_trackUrl[resolved_id])    
-
-    let route_pass = search_trackIDs[resolved_id] + ',' + search_trackNames[resolved_id] + ',' + search_trackPopularity[resolved_id]
-    localStorage.setItem('routePass',route_pass)
-        
-    input_hidden.value = route_pass
-}
-
-// var element = document.getElementById("card0");
-// element.addEventListener("click",function(){
-// alert("clicked");
-// })
-
-// const card1 = document.querySelector("#card1");
-// console.log(card1);
-
-// function release() {
-//     var offset = 0;
-//     var limit = 12;
-//     var market = 'US'
-
-//     var url = `${RELEASE_URL}?country=${market}&limit=${limit}&offset=${offset}`
-//     // console.log(url)
-//     response = fetch(url, {
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json',
-//             'Authorization': 'Bearer ' + access_token,
-//         }
-//     })
-//     .then(res => res.clone().json())
-//     .then(function (data) {
-//         // console.log(data)
-//         // localStorage.setItem("resp",data);
-//         return data;
-//     })
-//     .then(function (resp) {
-//         console.log(resp);
-
-//         let albums = resp['albums']
-//         let items = albums['items']
-//         // console.log(items[0])
-//         // console.log(items[0]['name'])
-//         for(let iter=0; iter<12; iter++){
-//             artists = items[iter]['artists']
-//             let str=''
-//             for(let i=0; i<artists.length; i++){
-//                 if(i==0){
-//                     str = str + artists[i]['name'];
-//                 }else{
-//                     str = str + ", " +artists[i]['name']
-//                 }
-//             }
-//             // // console.log(str) 
-//             // console.log(items[iter]['external_urls']['spotify'])
-//             // console.log(items[iter]['images'][1]['url'])
 
 
 
-//             let selector_id = `card${iter}`
-//             // console.log(selector_id)
-//             var x = document.getElementById(`${selector_id}`);
-//             x.querySelector("h5").innerHTML = items[iter]['name'];
-//             x.querySelector("p").innerHTML = str;
-//             x.querySelector("img").alt = items[iter]['name'];
-//             x.querySelector("img").src = items[iter]['images'][1]['url'];
-//             x.querySelector("a").href = items[iter]['external_urls']['spotify']; 
-//             x.querySelector("a").target = "_blank"; 
-
-//         } 
-        
-//     })
-// }
-// release();
 
 
-// let selector_id = `card${1}`
-// var x = document.getElementById(`${selector_id}`);
-// x.querySelector("h5").innerHTML = "TrackName";
-// x.querySelector("h5").innerHTML = "TrackName";
-// x.querySelector("p").innerHTML = "ArtistName";
-// x.querySelector("img").alt = "TrackImage";
-// x.querySelector("img").src = "https://i.scdn.co/image/ab67616d00001e02db2e767675059c95c7e85df8";
-// x.querySelector("a").href = "https://open.spotify.com/album/1OedsPZaNzUJILkvivIYF7";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
